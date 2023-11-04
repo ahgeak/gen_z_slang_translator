9ca14664b7580fea1ca3b1fc6a986198d3fe83a9
Anna Chuapetcharasopon <143050387+ahgeak@users.noreply.github.com>
2023-11-04 09:50:00 -0700

    JS: added local storage to store favorited list; HTML: added favorite list card and tools used sources

--- assets/script.js
+++ assets/script.js
@@ -40,3 +40,82 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   });
+
+  // https://developers.giphy.com/docs/api/#quick-start-guide
+var tempSearchWord = "penguin"; // this will change once connected to Bryan's code
+var tempSearchDefinition = "A cute animal that lives in the Antarctic"
+var tempGiphyApiKey = "u9ItgShSNZy4TjZz99RxhFbHCXXkvSMU"; // this will connect to the API key in keys.js
+var requestURL =
+  "https://api.giphy.com/v1/gifs/search?api_key=" +
+  tempGiphyApiKey +
+  "&limit=3&q=" +
+  tempSearchWord;
+console.log(requestURL);
+
+// Event listener for the "Make a Gif?" button
+var gifButton = document.querySelector(".gif-button");
+gifButton.addEventListener("click", searchGif);
+
+// searchGif funciton is called when "Make a Gif?" button is clicked. This function uses an API fetch request to return 3 gifs related to the inputed search word
+function searchGif() {
+  fetch(requestURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (content) {
+      $("#gif1").attr({
+        src: content.data[0].images.downsized.url,
+        alt: content.data[0].title,
+      });
+      $("#gif2").attr({
+        src: content.data[1].images.downsized.url,
+        alt: content.data[1].title,
+      });
+      $("#gif3").attr({
+        src: content.data[2].images.downsized.url,
+        alt: content.data[2].title,
+      });
+    });
+}
+
+
+// Event listener for the "Favorites List" button
+var favoriteButton = document.querySelector(".favorite-button");
+favoriteButton.addEventListener("click", storeFavoriteList);
+
+var storedFavoritesArray = {
+  word: "",
+  definition: ""
+}
+
+console.log(storedFavoritesArray);
+
+// var string = JSON.stringify(storedFavoritesArray);
+ 
+function storeFavorite(){
+  localStorage.setItem("word", tempSearchWord);
+  localStorage.setItem("definition", JSON.stringify(tempSearchDefinition));
+}
+
+function storeFavoriteList() {
+  // var tempObjectArray = {
+  //   word: tempSearchWord,
+  //   definition: tempSearchDefinition
+  // }
+  // storedFavoritesArray.push(JSON.stringify(tempObjectArray));
+
+  var favoriteListContent = document.getElementById("favorite-list-content");
+  var headerElement = document.createElement("h4");
+  var pElement = document.createElement("p");
+
+  favoriteListContent.appendChild(headerElement);
+  // headerElement.textContent = tempSearchWord;
+  headerElement.textContent = localStorage.getItem("word", tempSearchWord);
+  // headerElement.innerHTML = tempSearchWord;
+  
+  favoriteListContent.appendChild(pElement);
+  pElement.innerHTML = tempSearchDefinition;
+  storeFavorite();
+}
+
+var favoriteListButton = document.querySelector("modal-js-example")
