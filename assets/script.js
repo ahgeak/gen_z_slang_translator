bf10b650ce3d13e6af40a12e5d3bab84980e7019
bryancz1493 <bryancz1493@gmail.com>
2023-11-03 21:56:02 -0700

    HTML: Added some IDs; JS: Fix API

--- assets/script.js
+++ assets/script.js
@@ -42,59 +42,71 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-// Wiki section for Gen Z Slang
-$(function () {
-  var wikiTextOutput = document.querySelector("#wiki-output");
-  var wikiOutput = document.querySelector(".definition");
-  var wikiRequestUrl = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&format=json&titles=List_of_Generation_Z_slang&rvprop=content&origin=*';
+  // Wiki section for Gen Z Slang
+  $(function () {
+    // Global variables for wiki url and wiki input + output
+    var wikiRequestUrl = 'https://www.wikitable2json.com/api/List_of_Generation_Z_slang?lang=en&cleanRef=false';
+    var wikiTextOutput = document.querySelector("#wiki-output");
+    var wikiOutput = document.querySelector(".definition");
+    var wikiInput = document.querySelector("#wiki-input");
+    
+    // Fetch URL and then for respone
+      fetch(wikiRequestUrl)
+        .then(function (response) {
+          return response.json();
+        })
+        .then( (content) => {
+          // This console.log is working!! Use this!
+          // console.log(content[0][1][0]);
+          // console.log(content[0][1][1]);
+          var slangTerm = content[0][1][0];
+          var slangString = JSON.stringify(slangTerm);
+          if (userInput === slangString) {
+            $("#search-output").text(slangString);
+            console.log("match");
+          } else {
+            console.log("doesnt match");
+            console.log(userInput);
+          }
+          var definitionTerm = content[0][1][1];
+          var definitionString = JSON.stringify(definitionTerm);
+          $("#definition-output").text(definitionString);
+        })
 
-  // Fetch URL and then for respone
-  fetch(wikiRequestUrl)
-  .then(function(response){
-    return response.json();
-  })
-  .then(function(content) {
-    console.log(content);
-  })
-    // .then((response) => {
-    //   if (!response.ok) {
-    //     throw new Error(`HTTP error: ${response.status}`)
-    //   }
-    //   return response.text();
-    // })
-
-    // .then((text) => {
-    //   wikiOutput.wikiTextOutput = text;
-    // })
-
-    // .catch((error) => {
-    //   wikiOutput.wikiTextOutput = `Could not fetch verse: ${error}`;
-    // })
+    // Matrix for slang word and definition
+    // var slangMatrix = [
+    //   ["@me", "Pronounced 'at me.' Used on social media when someone feels attacked by a post."],
+    //   ["Asl", "Shortened version of 'as hell' Unrelated to early internet slang 'age/sex/location.'"],
+    //   ["Ate", "Used to express praise/admiration for a certion action or emotion performed by someone, with them often doing that action successfully."]
+    // ];
+    
+    // for (let i = 0; i < slangMatrix.length; i++) {
+    //   console.log('Term: ${slangMatrix[i][0]}, Definition: ${slangMatrix[i][1]}')
 
   // Function for wiki input to search
-  function clickToSearchForSlangWords(event) {
-    event.preventDefault();
-    var wikiInput = document.querySelector("#wiki-input");
-    // console.log(wikiInput.value);
-    var inputInSearchBox = document.querySelector(".input");
-    var userInput = wikiInput.value;
-  }
-  
-  var wikiSearchButton = document.querySelector(".searchBtn");
-  wikiSearchButton.addEventListener('click', clickToSearchForSlangWords);
-  
-  // Function + For loop to get wiki output from search 
-  // Maybe we dont need a function to display search output 
-  function wikiOutputFromSearch(event) {
-    event.preventDefault();
-    var outputInDefinitionArea = wikiOutput.val();
-  }
+    function clickToSearchForSlangWords(event) {
+      event.preventDefault();
+      // console.log(wikiInput.value);
+      var inputInSearchBox = document.querySelector(".input");
+      var userInput = wikiInput.value;
+      return userInput;
+      }
 
-  // Function to add wiki output to favorite
-  function clickToAddWikiFav(event) {
-    event.preventDefault();
-  }
-  
-  var wikiFavButton = document.querySelector(".favorite-button");
-  wikiFavButton.addEventListener('click', clickToAddWikiFav);
-});
+    var wikiSearchButton = document.querySelector("#searchBtn");
+    wikiSearchButton.addEventListener('click',clickToSearchForSlangWords);
+
+    // Function + For loop to get wiki output from search 
+    // Maybe we dont need a function to display search output 
+    function wikiOutputFromSearch(event) {
+      event.preventDefault();
+      var outputInDefinitionArea = wikiOutput.val();
+    }
+
+    // Function to add wiki output to favorite
+    function clickToAddWikiFav(event) {
+      event.preventDefault();
+    }
+
+    var wikiFavButton = document.querySelector(".favorite-button");
+    wikiFavButton.addEventListener('click', clickToAddWikiFav);
+  });
